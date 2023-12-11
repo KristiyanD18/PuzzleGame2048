e7b7f0789ec02875e9af31807a1f09043ecaa53d
Kris <Kris@DESKTOP-GCEKQ86>
2023-12-11 23:22:26 +0200

    Add project files.

---
+++ PuzzleGame2048/PuzzleGame2048.cpp
@@ -0,0 +1,21 @@
+/**
+*
+* Solution to course project # 4
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2023/2024
+*
+* @author Kristiyan Dimov
+* @idnumber 5MI0600358
+* @compiler VS
+*
+* <main code>
+*
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}

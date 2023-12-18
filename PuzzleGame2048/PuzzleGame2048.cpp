ab211faacf132c22b0299af19ec2bd0bea8263fc
Kristiyan Dimov <krisito2015@abv.bg>
2023-12-18 19:00:59 +0200

    Update PuzzleGame2048.cpp

--- PuzzleGame2048/PuzzleGame2048.cpp
+++ PuzzleGame2048/PuzzleGame2048.cpp
@@ -14,8 +14,67 @@
 */
 
 #include <iostream>
+using namespace std;
+
+bool isEqual(const char* firstWord, const char* secondWord)
+{
+	if (!firstWord || !secondWord)
+	{
+		return 0; //some error value
+	}
+
+	int index = 0;
+	while (firstWord[index] != '\0' && secondWord[index] != '\0')
+	{
+		if (firstWord[index] != secondWord[index])
+		{
+			return false;
+		}
+
+		index++;
+	}
+
+	return true;
+}
+
+void startGame() 
+{
+	constexpr int MAX_SIZE_INPUT = 1024;
+	char inputCommand[MAX_SIZE_INPUT];
+
+	std::cout << "Select command:\n1. Start game \n2. Leaderboard \n3. Quit" << std::endl;
+
+	while (true)
+	{
+		unsigned a = 1;
+		std::cout << "Input your selection here: ";
+		std::cin.getline(inputCommand, MAX_SIZE_INPUT);
+
+		if (isEqual(inputCommand, "Start game"))
+		{
+			// functions when given start game
+			std::cout << "start";
+		}
+		else if (isEqual(inputCommand, "Leaderboard"))
+		{
+			// functions when given leaderboard
+
+			std::cout << "leaderboard";
+		}
+		else if (isEqual(inputCommand, "Quit"))
+		{
+			// functions when given quit
+
+			std::cout << "quit";
+		}
+		else
+		{
+			continue;
+		}
+	}
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	startGame();
 }
